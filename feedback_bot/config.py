3643a53f7d6f155cd293ffa2e1db7dc60c53495f
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-29 02:35:58 +0300

    Update config.py

--- feedback_bot/config.py
+++ feedback_bot/config.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from feedback_bot.command_replies import Replies
 
-TG_TOKEN = os.environ["TG_BOT_TOKEN"]
+TG_TOKEN = os.environ["1327572686:AAFsgdpyubYSw6ZUiw0xQX6sbJ4rfra0o8I"]
 PROXY = os.environ.get("RT_HTTPS_PROXY")
-CHAT_ID = int(os.environ["TG_CHAT_ID"])
+CHAT_ID = int(os.environ["-470600717"])
 REPLIES: Replies = Replies.load_from_dir(Path(os.environ["COMMAND_REPLIES_PATH"]))
 BOT_TIMEOUT = int(os.environ.get("BOT_TIMEOUT", 10))
 BOT_RETRIES = int(os.environ.get("BOT_RETRIES", 5))
